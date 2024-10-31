82f056d4d9339a4966f25a612d749c7c597f1ecd
Pristine Shin <prisvsyo@gmail.com>
2024-10-31 13:20:17 -0700

    Completed basic testing/skeleton of review routes and made basic wtf forms for create/update review

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -7,26 +7,35 @@ from app.forms import NewProductForm
 
 review_routes = Blueprint('reviews', __name__)
 
-#add user verification before editing
+# Update and return an existing review
 @review_routes.route('/<int:reviewId>', methods=['PUT'])
 @login_required
 def editReview(reviewId):
   review = Review.query.get(reviewId)
-  if(review):
-    #edit the review
-    return "edit this"
-  else:
-    return {'message': 'Review not found!'}, 404
+  if review is None:
+    return {'message': 'Review could not be found!'}, 404
+  if(review.get_userId != current_user.id):
+    return {'message': 'Requires proper authorization!'}, 403
+  data = request.get_json()
+  if "review" in data:
+    review.review = data["review"]
+  try:
+    db.session.commit()
+    return {'review': review.to_dict()}
+  except Exception as e:
+    db.session.rollback()
+    return {'message': 'Error updating Product', 'error': str(e)}, 400
 
-
-#add user verification before deleting
+# Delete an existing Review
 @review_routes.route('/<int:reviewId>', methods=['DELETE'])
 @login_required
 def deleteReview(reviewId):
   review = Review.query.get(reviewId)
+  if review is None:
+    return {'message': 'Review could not be found!'}, 404
+  if(review.get_userId != current_user.id):
+      return {'message': 'Requires proper authorization!'}, 403
   if(review):
     db.session.delete(review)
     db.session.commit()
     return {"message": "Review successfully deleted"}
-  else:
-    return {'message': 'Review not found!'}, 404
