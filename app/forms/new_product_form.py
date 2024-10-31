3afef066e16ce9483041a81fa142728db67ed27c
Pristine Shin <prisvsyo@gmail.com>
2024-10-31 13:20:45 -0700

    Added selectfield values to type and genre for create/edit product forms

--- app/forms/new_product_form.py
+++ app/forms/new_product_form.py
@@ -6,8 +6,53 @@ from app.models import Product
 
 class NewProductForm(FlaskForm):
   name = StringField('name', validators=[DataRequired()])
-  type = SelectField('type', validators=[DataRequired()])
-  genre = SelectField('genre'),
+  type = SelectField(
+        'type',
+        choices=[
+            ('', 'Select...'),
+            ('music', '--- Music ---'),
+            ('cd', 'Compact Disc (CD)'),
+            ('cassette', 'Cassette'),
+            ('vinyl_lp', 'Vinyl LP'),
+            ('double_vinyl_lp', '2 x Vinyl LP'),
+            ('vinyl_7', '7" Vinyl'),
+            ('vinyl_box_set', 'Vinyl Box Set'),
+            ('other_vinyl', 'Other Vinyl'),
+
+            ('apparel', '--- Apparel ---'),
+            ('t_shirt', 'T-Shirt/Shirt'),
+            ('sweater_hoodie', 'Sweater/Hoodie'),
+            ('hat', 'Hat'),
+            ('other_apparel', 'Other Apparel'),
+
+            ('miscellaneous', '--- Miscellaneous ---'),
+            ('dvd', 'DVD'),
+            ('usb_flash_drive', 'USB Flash Drive'),
+            ('sheet_music', 'Sheet Music'),
+            ('poster_print', 'Poster/Print'),
+            ('ticket', 'Ticket'),
+            ('book_magazine', 'Book/Magazine'),
+            ('button_pin_patch', 'Button/Pin/Patch'),
+            ('bag', 'Bag'),
+            ('other', 'Other'),
+        ],
+        validators=[DataRequired()]
+    )
+  genre = SelectField(
+        'genre',
+        choices=[
+            ('', 'Select...'),
+            ('electronic', 'Electronic'),
+            ('metal', 'Metal'),
+            ('rock', 'Rock'),
+            ('alternative', 'Alternative'),
+            ('hip_hop_rap', 'Hip-Hop/Rap'),
+            ('experimental', 'Experimental'),
+            ('punk', 'Punk'),
+            ('pop', 'Pop'),
+            ('ambient', 'Ambient'),
+        ]
+    )
   price = DecimalField('price', validators=[DataRequired(), NumberRange(min=0.01, message='Price must be a positive number!')])
   decription = StringField('description', validators=[DataRequired()])
   imageUrl = URLField('imageUrl', validators=[DataRequired(), URL()])
