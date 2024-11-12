bb067b6b7ba6ca1d5341b4c7b824d9adf90c3524
savonnas <savonna.sou@gmail.com>
2024-11-11 21:56:14 -0500

    remove 4 of checkoutpage of csrf

--- react-vite/src/components/CheckoutPage/CheckoutPage.jsx
+++ react-vite/src/components/CheckoutPage/CheckoutPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./CheckoutPage.css";
-import { csrfFetch } from "../../redux/csrf";
 
 function CheckoutPage() {
     const dispatch = useDispatch();
