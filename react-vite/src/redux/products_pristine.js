0d653c3f43c22b424c2170ce97a5ac5df384a871
Pristine <prisvsyo@gmail.com>
2024-11-14 13:27:25 -0800

    added try/catches to products and reviews thunks

--- react-vite/src/redux/products_pristine.js
+++ react-vite/src/redux/products_pristine.js
@@ -84,24 +84,42 @@ export const deleteProduct = productId => (
 export const thunkGetAllProducts = () => async dispatch => {
     // dispatch(loadAllProductsReq())
 
-    const res = await fetch('/api/products');
-    if (res.ok) {
-        const products = await res.json()
-        // console.log(`Testing thunkGetAllProducts: ${products}`)
-        if (products.errors) return products.errors
-        dispatch(loadAllProducts(products["products"]))
+    try {
+        const res = await fetch('/api/products');
+        if (res.ok) {
+            const products = await res.json()
+            if (products.errors) return products.errors
+            dispatch(loadAllProducts(products["products"]))
+        }
+        else if (res.status < 500) {
+            const errorMessages = await res.json();
+            console.error("Validation Errors:", errorMessages);
+            return errorMessages
+        }
+    } catch (e) {
+        console.error("Error in thunkGetAllProducts:", e);
+        return { server: "Something went wrong. Please try again" }
     }
 }
 
 export const thunkGetLimitedProducts = (limit = 20) => async dispatch => {
     // dispatch(loadLimitedProductsReq())
-
-    const res = await fetch(`/api/products?limit=${limit}`);
-    if (res.ok) {
-        const products = await res.json()
-        // console.log(`Testing thunkGetLimitedProducts: ${JSON.stringify(products)}`)
-        if (products.errors) return products.errors
-        dispatch(loadLimitedProducts(products["products"]))
+    try {
+        const res = await fetch(`/api/products?limit=${limit}`);
+        if (res.ok) {
+            const products = await res.json()
+            // console.log(`Testing thunkGetLimitedProducts: ${JSON.stringify(products)}`)
+            if (products.errors) return products.errors
+            dispatch(loadLimitedProducts(products["products"]))
+        }
+        else if (res.status < 500) {
+            const errorMessages = await res.json();
+            console.error("Validation Errors:", errorMessages);
+            return errorMessages
+        }
+    } catch (e) {
+        console.error("Error in thunkGetLimitedProducts:", e);
+        return { server: "Something went wrong. Please try again" }
     }
 }
 
@@ -115,40 +133,54 @@ export const thunkGetLimitedProducts = (limit = 20) => async dispatch => {
 // }
 
 export const thunkGetProductById = productId => async dispatch => {
-    const res = await fetch(`/api/products/${productId}`);
-    if (res.ok) {
-        const product = await res.json()
-        // console.log(`Testing thunkGetProductById: ${JSON.stringify(product)}`)
-        if (product.errors) return product.errors
-        dispatch(loadProductById(product))
-        return product
+    try {
+        const res = await fetch(`/api/products/${productId}`);
+        if (res.ok) {
+            const product = await res.json()
+            // console.log(`Testing thunkGetProductById: ${JSON.stringify(product)}`)
+            if (product.errors) return product.errors
+            dispatch(loadProductById(product))
+            return product
+        }
+        else if (res.status < 500) {
+            const errorMessages = await res.json();
+            console.error("Validation Errors:", errorMessages);
+            return errorMessages
+        }
+    } catch (e) {
+        console.error("Error in thunkGetProductById:", e);
+        return { server: "Something went wrong. Please try again" }
     }
 }
 
 export const thunkAddProduct = (product) => async dispatch => {
-    const response = await fetch("/api/products/", {
-        method: "POST",
-        // headers: { "Content-Type": "application/json" },
-        // need to stringify body rather than write directly when in backend because its python going to js
-        // body: JSON.stringify(product)
-        body: product
-    });
-
-    if (response.ok) {
-        const newProduct = await response.json();
-        dispatch(createProduct(newProduct));
-    } else if (response.status < 500) {
-        const errorMessages = await response.json();
-        console.error("Validation Errors:", errorMessages);
-        return errorMessages
-    } else {
+    try {
+        const res = await fetch("/api/products/", {
+            method: "POST",
+            // headers: { "Content-Type": "application/json" },
+            // need to stringify body rather than write directly when in backend because its python going to js
+            // body: JSON.stringify(product)
+            body: product
+        });
+        if (res.ok) {
+            const newProduct = await res.json();
+            dispatch(createProduct(newProduct));
+        }
+        else if (res.status < 500) {
+            const errorMessages = await res.json();
+            console.error("Validation Errors:", errorMessages);
+            return errorMessages
+        }
+    } catch (e) {
+        console.error("Error in thunkAddProduct:", e);
         return { server: "Something went wrong. Please try again" }
     }
+
 };
 
 export const thunkEditProduct = (product) => async dispatch => {
     try {
-        console.log(`Testing product payload before fetch: ${JSON.stringify(product)}`)
+        // console.log(`Testing product payload before fetch: ${JSON.stringify(product)}`)
         const productId = product.get("id");
 
         const editRes = await fetch(`/api/products/edit/${productId}`,
@@ -179,17 +211,26 @@ export const thunkEditProduct = (product) => async dispatch => {
 }
 
 export const thunkRemoveProduct = productId => async dispatch => {
-    const res = await fetch(`/api/products/${productId}`,
-        {
-            method: 'DELETE',
-            headers: { 'Content-Type': 'application/json' }
+    try {
+        const res = await fetch(`/api/products/${productId}`,
+            {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' }
+            }
+        )
+        if (res.ok) {
+            const deleted = await res.json();
+            if (deleted.errors) return deleted.errors
+            dispatch(deleteProduct(productId))
         }
-    )
-
-    if (res.ok) {
-        const deleted = await res.json();
-        if (deleted.errors) return deleted.errors
-        dispatch(deleteProduct(productId))
+        else if (res.status < 500) {
+            const errorMessages = await res.json();
+            console.error("Validation Errors:", errorMessages);
+            return errorMessages
+        }
+    } catch (e) {
+        console.error("Error in thunkRemoveProduct:", e);
+        return { server: "Something went wrong. Please try again" }
     }
 }
 
@@ -207,16 +248,26 @@ export const thunkGetProductReviews = productId => async dispatch => {
 }
 
 export const thunkAddAProductReview = (productId, review) => async dispatch => {
-    const res = await fetch(`/api/products/${productId}/reviews`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(review)
-    })
-    if (res.ok) {
-        const newReview = await res.json()
-        if (newReview.errors) return newReview.errors
-        dispatch(createProductReview(newReview.review))
-        return res
+    try {
+        const res = await fetch(`/api/products/${productId}/reviews`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(review)
+        })
+        if (res.ok) {
+            const newReview = await res.json()
+            if (newReview.errors) return newReview.errors
+            dispatch(createProductReview(newReview.review))
+            return res
+        }
+        else if (res.status < 500) {
+            const errorMessages = await res.json();
+            console.error("Validation Errors:", errorMessages);
+            return errorMessages
+        }
+    } catch (e) {
+        console.error("Error in thunkAddAProductReview:", e);
+        return { server: "Something went wrong. Please try again" }
     }
 }
 
@@ -304,7 +355,7 @@ function productsReducer(state = initialState, action) {
             return { ...state }
         }
         case CREATE_PRODUCT_REVIEW: {
-            const {productId} = action.review
+            const { productId } = action.review
             const review = action.review
             const currentReviews = state.allProducts[productId]?.reviews || {};
             return {
