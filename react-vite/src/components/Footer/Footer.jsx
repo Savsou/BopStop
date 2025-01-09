880fb30e4190dd171d30372cfe96feac21e76b3a
savonnas <savonna.sou@gmail.com>
2025-01-08 22:51:35 -0500

    edit footer for socials

--- react-vite/src/components/Footer/Footer.jsx
+++ react-vite/src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 // import React from "react";
-import "./Footer.css"; 
+import "./Footer.css";
 import logowhite from "../../../src/logo-white.png";
+import linkedinLogo from "../../../../logos/linkedin-logo.png";
+import githubLogo from "../../../../logos/github-logo.png";
 
 const Footer = () => {
   return (
@@ -14,6 +16,51 @@ const Footer = () => {
           <p className="link">Privacy</p>
           <p className="link">Copyright</p>
           <p className="link">Acceptable Use & Moderation Policy</p>
+        </div>
+      </div>
+      <a href="https://github.com/Savsou/BopStop">
+        <img src={githubLogo} alt="github-logo" className="social-icon github-icon" />
+      </a>
+      <div className="social-medias">
+        <div className="socials">
+          <p>Pristine&apos;s Socials:</p>
+          <a
+            href="https://www.linkedin.com/in/pristine-shin/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinLogo} alt="pristine-linkedin" className="social-icon" />
+          </a>
+        </div>
+        <div className="socials">
+          <p>Tiffany&apos;s Socials:</p>
+          <a
+            href="https://www.linkedin.com/in/ittseng/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinLogo} alt="tiffany-linkedin" className="social-icon" />
+          </a>
+        </div>
+        <div className="socials">
+          <p>Zechariah&apos;s Socials:</p>
+          <a
+            href="https://www.linkedin.com/in/zechariah-dominguez/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinLogo} alt="zechariah-linkedin" className="social-icon" />
+          </a>
+        </div>
+        <div className="socials">
+          <p>Savannah&apos;s Socials:</p>
+          <a
+            href="https://www.linkedin.com/in/pristine-shin/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinLogo} alt="savannah-linkedin" className="social-icon" />
+          </a>
         </div>
       </div>
     </footer>
